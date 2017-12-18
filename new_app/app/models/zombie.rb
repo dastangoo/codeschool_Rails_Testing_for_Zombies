dfbc60e1442be8610bcfb26e5ba87c0c76a38b34
dastangoo <hamed.dastangoo@gmail.com>
2017-12-18 20:18:49 +0330

    Mocks and Stubs

--- new_app/app/models/zombie.rb
+++ new_app/app/models/zombie.rb
@@ -1,11 +1,19 @@
 class Zombie < ApplicationRecord
-  validates :name, presence: true
-   
-  def avatar_url
-    "http://zombitar.com/#{id}.jpg"
+  has_one :weapon
+  
+  def decapitate
+     weapon.slice(self, :head)
+     self.status = "dead again"
   end
-  def tweets
-    
+  
+  def geolocate
+    loc = Zoogle.graveyard_locator(self.graveyard)
+    "#{loc[:latitude]}, #{loc[:longitude]}"
+  end
+  
+  def geolocate_with_object
+    loc = Zoogle.graveyard_locator(self.graveyard)
+    "#{loc.latitude}, #{loc.longitude}"
   end
 end
  
--- new_app/test/models/zombie_test.rb
+++ new_app/test/models/zombie_test.rb
@@ -1,43 +1,28 @@
 require 'test_helper'
 
 class ZombieTest < ActiveSupport::TestCase
-  
-  def setup
-    @z = zombies(:ash)
+  test "decapitate should set status to dead again" do
+    @zombie.weapon.stubs(:slice)
+    @zombie.decapitate
+    assert_equal "dead again", @zombie.status 
+  end
+  test "decapitate should call slice" do
+    @zombie.weapon.expects(:slice)
+    @zombie.decapitate
   end
   
-  def assert_presence(model, field)
-    model.valid?
-    assert_match /can't be blank/, model.errors[field].join, "Presence error for #{field} not found on #{model.class}"
-  end
-  # test "should contain only tweets that belong to zombie" do
-  #   assert z.tweets.all? { |t| t.zombie == z }
-  # end
-  
-  # test "can generate avatar_url" do
-  #   assert_equal "http://zombitar.com/#{z.id}.jpg", z.avatar_url
-  # end
-  
-  test "invalid name gives error message" do
-    @z.name = nil
-    # assert_presence(@z, :name)
-    should validate_presence_of(:name) 
-    # should validate_presence_of(:graveyard)
-    # should ensure_length_of(:name).is_at_most(15)
-    # should have_many(:tweets)
-    # should validate_uniqueness_of(:name)
-    # should ensure_length_of(:password).is_at_least(5).is_at_most(20)
-    # should validate_numericality_of(:age)
-    # should_not allow_value("blah").for(:email)
-    # should allow_value("a@b.com").for(:email)
-    # should ensure_inclusion_of(:age).in_range(1..10)
-    # should_not allow_mass_assignment_of(:password)
-    # should belong_to(:zombie)
-    # should validate_acceptance_of(:terms_of_service )
+  test "geolocate calls the Zoogle graveyard locator" do
+    Zoogle.expects(:graveyard_locator).with(@zombie.graveyard).returns({latitude: 2, longitude: 3 })
+    @zombie.geolocate
   end
   
-  test "invalid graveyard gives error message" do
-    @z.graveyard = nil
-    assert(@z, :graveyard)
+  test "geolocate returns properly formatted lat, long" do
+    Zoogle.stubs(:graveyard_locator).with(@zombie.graveyard).returns({latitude: 2, longitude: 3})
+    assert_equal "2, 3", @zombie.geolocate
+  end
+  test "geolocate_with_object properly formatted lat, long" do
+    loc = stub(latitude: 2, longitude: 3)
+    Zoogle.stubs(:graveyard_locator).returns(loc)
+    assert_equal "2, 3", @zombie.geolocate_with_object
   end
 end
